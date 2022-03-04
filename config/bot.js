145bc9c459b32b1b3676b750c8e350d752ab3cab
dyxzkh168 <97222089+dyxzkh168@users.noreply.github.com>
2022-03-04 23:47:54 +0700

    Music Bot

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: '🔴',
+        error: '🟡',
+        queue: '💌',
+        music: '🔰',
+        success: '🟢',
+    },
+
+    discord: {
+        token: 'OTM2Mjc5NzQ3NDE0MDc3NTcx.YfK4dw.c7mmWtGjoA6Gm_nbGU1s2dwSL4k',
+        prefix: '.',
+        activity: 'Joy Knea',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
